231a13521464f8a7f67a4980317d6aea2955549f
hassaammgl <h455a4mmehtab@gmail.com>
2025-04-05 14:09:34 +0500

    fix: Adjust footer positioning and enhance GitHub button styling in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,13 @@ const Hero = () => {
                     className="absolute grayscale-100 hover:grayscale-0 transition-all ease-in-out duration-500 -bottom-10 z-20 max-h-[80vh] object-contain drop-shadow-black/100"
                 />
             </div>
-            <div className="absolute flex items-center justify-between text-sm text-gray-700 bottom-20 left-0 px-16 py-4 font-sans w-full ">
+            <div className="absolute flex items-center justify-between text-sm text-gray-700 bottom-0 left-0 px-16 py-4 font-sans w-full ">
                 <p>I started with <span className="font-semibold text-green-600">curiosity</span>, just trying to build something cool. <br />
                     What began as simple HTML turned into <span className="font-semibold text-green-600">full-stack apps</span> <br /> powered by <span className="font-semibold">React, MongoDB, and more</span>. <br />
                     Now, I code not just to create — but to <span className="font-semibold text-green-600">solve, innovate, and inspire</span></p>
-                <a target='_blank' href='https://github.com/hassaammgl/'><button></button> <FaGithub /></a>
+                <div className="flex items-center gap-4 font-bold text-gray-700 bg-black px-2 py-2 rounded-full hover:bg-gray-800 hover:scale-105 transform transition-all ease-in-out duration-300 cursor-pointer animate-pulse hover:animate-none">
+                    <FaGithub className='text-white text-2xl hover:rotate-12 transition-transform duration-300' /> <span className="text-white">Github</span>
+                </div>
             </div>
         </section>
     );
